4c8d3d293d3dea362cfe0d1b5c7967ca4399a761
letanh215 <letanh215@gmail.com>
2026-03-01 20:18:06 +0800

    Updating SBG + Velodynes

--- sensor_kit/nuway_sensor_kit_launch/nuway_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
+++ sensor_kit/nuway_sensor_kit_launch/nuway_sensor_kit_launch/launch/pointcloud_preprocessor.launch.py
@@ -66,7 +66,7 @@ def generate_launch_description():
     def add_launch_arg(name: str, default_value=None):
         launch_arguments.append(DeclareLaunchArgument(name, default_value=default_value))
 
-    sample_sensor_kit_launch_share_dir = get_package_share_directory("sample_sensor_kit_launch")
+    nuway_sensor_kit_launch_share_dir = get_package_share_directory("nuway_sensor_kit_launch")
 
     add_launch_arg("base_frame", "base_link")
     add_launch_arg("use_multithread", "False")
@@ -75,7 +75,7 @@ def generate_launch_description():
     add_launch_arg(
         "concatenate_and_time_sync_node_param_path",
         os.path.join(
-            sample_sensor_kit_launch_share_dir,
+            nuway_sensor_kit_launch_share_dir,
             "config",
             "concatenate_and_time_sync_node.param.yaml",
         ),
